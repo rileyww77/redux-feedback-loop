3cc42a6eb4267cb595c07cac961dcd632771b1a4
riley_ww77 <riley_ww77@yahoo.com>
2020-08-22 11:49:22 -0500

    finished base setup. all commponents made. all wireframes created. index base set up

---
+++ src/components/Feelings/Feelings.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feelings extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Feelings;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+//redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { logger } from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+    combineReducers({
+        
+    }),
+    applyMiddleware(logger)
+);
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
