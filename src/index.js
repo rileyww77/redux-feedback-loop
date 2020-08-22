fe5eb73fa91ad040778a2022d8e21a4f6abefc1a
riley_ww77 <riley_ww77@yahoo.com>
2020-08-22 12:30:22 -0500

    added reducers in index

--- src/index.js
+++ src/index.js
@@ -8,10 +8,50 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { logger } from 'redux-logger'
 
+const feelingsReducer = (state = [], action) => {
+    if (action.type === 'FEELINGS'){
+        return action.payload
+    } else if (action.type === 'SUBMIT') {
+        return []
+    }
+    return state;
+}
+
+const understandingReducer = (state = [], action) => {
+    if (action.type === 'UNDERSTANDING'){
+        return action.payload
+    } else if (action.type === 'SUBMIT') {
+        return []
+    }
+    return state;
+}
+
+const supportReducer = (state = [], action) => {
+    if (action.type === 'SUPPORT'){
+        return action.payload
+    } else if (action.type === 'SUBMIT') {
+        return []
+    }
+    return state;
+}
+
+const commentsReducer = (state = [], action) => {
+    if (action.type === 'COMMENTS'){
+        return action.payload
+    } else if (action.type === 'SUBMIT') {
+        return []
+    }
+    return state;
+}
+
+
 
 const store = createStore(
     combineReducers({
-        
+        feelingsReducer,
+        understandingReducer,
+        commentsReducer,
+        supportReducer
     }),
     applyMiddleware(logger)
 );
